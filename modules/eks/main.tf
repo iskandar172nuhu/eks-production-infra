f790cbbaf7e910837901dbcb784b1c277dab9e40
Iskandar Nuhu <iskandar172nuhu@gmail.com>
2026-04-09 01:55:00 +0100

    feat(k8s): deploy app to EKS

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -88,8 +88,8 @@ resource "aws_eks_node_group" "this" {
 
   scaling_config {
     desired_size = 2
-    max_size     = 2
-    min_size     = 1
+    max_size     = 3
+    min_size     = 2
   }
 
   instance_types = ["t3.micro"]
